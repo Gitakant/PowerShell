8837b0b17edf7ba2108eb2b5019df94ad4c92ece
Gitakant <118975183+Gitakant@users.noreply.github.com>
2025-01-17 18:11:44 +0530

    Add files via upload

---
+++ ResourceAutomation.ps1
@@ -0,0 +1,57 @@
+﻿
+[CmdletBinding()]
+param(
+  [parameter(Mandatory=$True,
+             ValueFromPipeline=$True,
+             ValueFromPipelineByPropertyName=$True,
+             HelpMessage="Enter the resource group name as per standard naming conventions")]
+  [string]$ResourceGroupName,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the location")]
+  [string]$Location,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the Portfolio")]
+  [string]$Portfolio,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the Environment")]
+  [string]$Environment,
+  [parameter(Mandatory=$True,
+             HelpMessage="Provide the name of application")]
+  [string]$Application,
+  [parameter(Mandatory=$True,
+             HelpMessage="Provide the name of project")]
+  [string]$Project,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the Business Criticality")]
+  [string]$BusinessCriticality,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter classification of data as per business requirements")]
+  [string]$DataClassification,
+  [parameter(Mandatory=$True,
+             HelpMessage="Please provide the business application owner contact")]
+  [string]$ContactEmail,
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the value of IO number for billing")]
+  [string]$BillTo,
+  [parameter(Mandatory=$True,
+             HelpMessage="The date on which this Resource Group was provisioned")]
+  [string]$TimeProvisioned,
+  #Storing in order
+  [Hashtable]$Tags = [ordered]@{
+    'Portfolio'=$Portfolio
+    'Environment'=$Environment
+    'Application'=$Application
+    'Project'=$Project
+    'BusinessCriticality'=$BusinessCriticality
+    'DataClassification'=$DataClassification
+    'ContactEmail'=$ContactEmail
+    'BillTo'=$BillTo
+    'TimeProvisioned'=$TimeProvisioned
+    
+}
+
+)
+Write-Output "Resource Group provisioing will start"
+$RG= New-AzResourceGroup -ResourceGroupName $ResourceGroupName.ToUpper() -Location $Location -Tag $Tags -Verbose
+Write-Host "$($RG.ResourceGroupName) : Resource Group provisioned in $($RG.Location) location" -ForegroundColor Green
+Write-Output "$($RG.ResourceGroupName) : Resource Group provisioned in $($RG.Location) location"
---
+++ UpdateAztag_testtt.ps1
@@ -0,0 +1,53 @@
+﻿
+<#
+.SYNOPSIS
+Update the ContactEmail tag on provided resource group
+.DESCRIPTION
+Script will update the ContactEmail tag on provided resource group
+testing
+.PARAMETER SubscriptionName - Provide the subscription where resource groups resides
+    ResourceGroupName - Provide the resource group where tag needs to be updated
+.EXAMPLE
+.\UpdateAztag_testtt.ps1 -SubscriptionName "GITAKANT-PAYASYOUGO-BRICICI" -ResourceGroupName (get-Content -Path .\rg.csv) -ShowProgress -Verbose
+#>
+
+[CmdletBinding()]
+param(
+  [parameter(Mandatory=$True,
+             HelpMessage="Enter the subscription name")]
+  [string[]]$SubscriptionName,
+  [parameter(Mandatory=$True,
+             ValueFromPipeline=$True,
+             ValueFromPipelineByPropertyName=$True,
+             HelpMessage="Enter the resource group name")]
+  [string[]]$ResourceGroupName,
+  [switch]$ShowProgress
+  
+)
+
+        foreach($Subscription in $SubscriptionName)
+        {
+        Write-Output -Message "Selecting subscription $($Subscription)"
+        Select-AzSubscription -SubscriptionName $Subscription -Verbose
+           
+                        foreach($ResourceGroup in $ResourceGroupName)
+                        {    
+
+                            #Get RG where we need to update tag value
+                            Write-Output -Message "Selecting Resource Group where we need to update tag $($ResourceGroup)"
+                            $RG = Get-AzResourceGroup -Name $ResourceGroup -Verbose
+
+                            $tags = @{"ContactEmail" = "jyotigitakantmungal@gmail.com"}
+
+                            Write-Output -Message "Replacing the ContactEmail Tag value to bhagwatmungal@gmail.com"
+                            if($tags.Count -ne 0)
+                            {
+                            Update-AzTag -ResourceId $RG.ResourceId -Tag $tags -Operation Merge -Verbose
+                            
+                            Write-Output "Tag updated successfully on " $RG.ResourceGroupName
+                            }
+                            else
+                            {Write-Output "No tags to be updated"}
+                        }
+
+            }
